9a829146996c1eda9bede0d9ccfd317792194b00
Andrea Iglio <andrea.iglio@gmail.com>
2021-03-03 16:45:27 +0100

    Implemented Novels and Articles tables

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,10 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-md-4">
             <ul class="list-unstyled">
-                <li><a href="#"><i class="fas fa-film"></i> Movies</a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
+                <li><a href="#"><i class="fas fa-book-reader"></i> Novels</a></li>
+                <li><a href="#"><i class="fas fa-film"></i> Articles</a></li>
+                <li><a href="#"><i class="fas fa-paint-brush"></i> Artists</a></li>
+                <li><a href="#"><i class="fas fa-pencil-alt"></i> Writers</a></li>
+                <li><a href="#"><i class="fas fa-newspaper"></i> Series_Novels</a></li>
+                <li><a href="#"><i class="fas fa-newspaper"></i> Series_Articles</a></li>
+
             </ul>
         </div>
         <div class="col-md-8">
@@ -17,9 +20,9 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     {{ __('You are logged in!') }}
