2da3ffb904f727db51474771d4e503dcd2662cd2
Andrea Iglio <andrea.iglio@gmail.com>
2021-03-05 16:59:14 +0100

    Completed main structure and style for homepage and novel->show() still need to make general details improvements

--- resources/views/guests/partials/navbar_bottom.blade.php
+++ resources/views/guests/partials/navbar_bottom.blade.php
@@ -5,11 +5,11 @@
             <img src="{{asset('img/navbar_top/logo.png')}}" alt="">
 
         </div>
-        
+
         <div class="center flex">
             <ul class="flex">
                 <li><a href="">CHARACTERS</a></li>
-                <li><a href="">COMICS</a></li>
+                <li class="active"><a href="">COMICS</a></li>
                 <li><a href="">MOVIES</a></li>
                 <li><a href="">TV</a></li>
                 <li><a href="">GAMES</a></li>
@@ -19,12 +19,12 @@
             </ul>
 
         </div>
-        
-        
+
+
         <div class="right flex">
-        
-        <input type="text" placeholder="Search">
-        <img src="{{asset('img/navbar_top/search-icon-new.png')}}" alt="">
+
+            <input type="text" placeholder="Search">
+            <img src="{{asset('img/navbar_top/search-icon-new.png')}}" alt="">
         </div>
     </div>
 
--- resources/views/guests/partials/navbar_top.blade.php
+++ resources/views/guests/partials/navbar_top.blade.php
@@ -5,7 +5,7 @@
 
     <div class="right flex">
         <ul class="flex">
-            <li><a href=""><img src="{{asset('img/navbar_top/DC.svg')}}" style="" alt=""> </a></li>
+            <li style="background-color:white"><a href=""><img src="{{asset('img/navbar_top/DC.svg')}}" style="background-color:white" alt=""> </a></li>
 
             <li><a href=""><img src="{{asset('img/navbar_top/DC_SHOP_desktop.svg')}}" alt=""></a></li>
             <li><a href=""><img src="{{asset('img/navbar_top/DCUI_desktop.svg')}}" alt=""></a></li>
